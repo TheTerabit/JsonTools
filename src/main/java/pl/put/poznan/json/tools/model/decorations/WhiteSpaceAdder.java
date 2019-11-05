34cf3521f6fbafa4d77794bfc83e963691740dca
Iwo Naglik <iwonaglik@o2.pl>
2019-11-05 19:32:40 +0100

    Implementation For json white space adder

--- src/main/java/pl/put/poznan/json/tools/model/decorations/WhiteSpaceAdder.java
+++ src/main/java/pl/put/poznan/json/tools/model/decorations/WhiteSpaceAdder.java
@@ -3,16 +3,20 @@ package pl.put.poznan.json.tools.model.decorations;
 import pl.put.poznan.json.tools.model.JsonDecorator;
 import pl.put.poznan.json.tools.model.JsonObject;
 
+
 public class WhiteSpaceAdder extends JsonDecorator {
 
     public WhiteSpaceAdder(JsonObject jsonObject) {
         super(jsonObject);
     }
 
-    public String getJson(){
-        //TODO
-        //tu zmienic jsonObject.getJson() na taki ze spacjami i zreturnowac
-        return jsonObject.getJson().toUpperCase();
+    public String getJson() {
+        //tu zmienic jsonObject.getJson() na taki ze spacjami i zreturnowac/
+        return jsonObject.getJson()
+                .replaceAll("\":", "\": ")
+                .replaceAll("\",", "\",\n")
+                .replaceAll("\\{", "\\{\n")
+                .replaceAll("\"\\}", "\"\n\\}");
     }
 
 }
