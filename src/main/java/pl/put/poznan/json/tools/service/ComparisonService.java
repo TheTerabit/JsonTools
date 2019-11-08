37a6c78f99e284e3c3e5762a779eccd69865201a
Bartosz Przybyl <bartolomeo369@gmail.com>
2019-11-09 00:47:43 +0100

    Add first implementation of ComparisonService

--- src/main/java/pl/put/poznan/json/tools/service/ComparisonService.java
+++ src/main/java/pl/put/poznan/json/tools/service/ComparisonService.java
@@ -9,6 +9,27 @@ import java.util.List;
 public class ComparisonService {
 
     public List<Integer> compare(String s, String s1) {
-        return new ArrayList<Integer>();
+        s = s.trim();
+        s1 = s1.trim();
+        if (s.equals(s1))
+            return new ArrayList<Integer>();
+        else {
+            String[] sSplitted = s.split("\\r?\\n");
+            String[] s1Splitted = s1.split("\\r?\\n");
+            return sSplitted.length > s1Splitted.length ? makeList(sSplitted, s1Splitted) : makeList(s1Splitted, sSplitted);
+        }
+    }
+
+    private List<Integer> makeList(String[] sSplitted, String[] s1Splitted) {
+        ArrayList<Integer> differences = new ArrayList<Integer>();
+        for (int i = 0; i < sSplitted.length; i++) {
+            if (i < s1Splitted.length) {
+                if (!sSplitted[i].equals(s1Splitted[i]))
+                    differences.add(i + 1);
+            } else
+                differences.add(i + 1);
+        }
+        return differences;
     }
 }
+
