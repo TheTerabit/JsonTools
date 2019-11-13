49f3f46a3cd8ed155307efc4e495a91162130b33
Bartosz Przybyl <bartolomeo369@gmail.com>
2019-11-13 21:08:00 +0100

    Add first implementation of AttributesPicker

--- src/main/java/pl/put/poznan/json/tools/model/decorations/AttributesPicker.java
+++ src/main/java/pl/put/poznan/json/tools/model/decorations/AttributesPicker.java
@@ -1,5 +1,8 @@
 package pl.put.poznan.json.tools.model.decorations;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import pl.put.poznan.json.tools.model.JsonDecorator;
 import pl.put.poznan.json.tools.model.JsonObject;
@@ -15,9 +18,28 @@ public class AttributesPicker extends JsonDecorator {
     }
 
     public String getJson() throws WrongInputException, ParseException {
-        //TODO
-        //tu zmienic jsonObject.getJson() na taki tylko z wybranymi atrybutami
-        return jsonObject.getJson();
+        return pickAttributes(jsonObject.getJson());
     }
 
+    private String pickAttributes(String json) throws ParseException {
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) parser.parse(json);
+        return parseAttributes(jsonObject);
+    }
+
+    private String parseAttributes(JSONObject json) {
+        StringBuilder output = new StringBuilder("{\n\t");
+        Object obj;
+        for (String attribute : attributes) {
+            obj = json.get(attribute);
+            if (obj instanceof Integer || obj instanceof Long || obj instanceof Float || obj instanceof Double || obj instanceof Boolean)
+                output.append("\"").append(attribute).append("\": ").append(obj).append("\n\t");
+            else if (obj instanceof String)
+                output.append("\"").append(attribute).append("\": ").append("\"").append(obj).append("\"\n\t");
+            else if (obj instanceof JSONArray || obj instanceof JSONObject)
+                output.append("\"").append(attribute).append("\": ").append(obj.toString()).append("\n\t");
+        }
+        output.append("}");
+        return output.toString().replace("\t}","}");
+    }
 }
