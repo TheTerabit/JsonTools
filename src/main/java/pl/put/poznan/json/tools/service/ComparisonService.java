68ba784265b51ecb14591d9eb5fb04c623ffd371
Bartosz Przybyl <bartolomeo369@gmail.com>
2019-11-13 19:28:08 +0100

    Little refactor of ComparisonService

--- src/main/java/pl/put/poznan/json/tools/service/ComparisonService.java
+++ src/main/java/pl/put/poznan/json/tools/service/ComparisonService.java
@@ -8,23 +8,36 @@ import java.util.List;
 @Service
 public class ComparisonService {
 
-    public List<Integer> compare(String s, String s1) {
-        s = s.trim();
-        s1 = s1.trim();
-        if (s.equals(s1))
+    private String[] json1;
+    private String[] json2;
+
+    private String trimJson(String json) {
+        return json.trim();
+    }
+
+    private String[] splitJson(String json) {
+        return json.split("\\r?\\n");
+    }
+
+    public List<Integer> compare(String json1, String json2) {
+        json1 = trimJson(json1);
+        json2 = trimJson(json2);
+        if (json1.equals(json2))
             return new ArrayList<Integer>();
         else {
-            String[] sSplitted = s.split("\\r?\\n");
-            String[] s1Splitted = s1.split("\\r?\\n");
-            return sSplitted.length > s1Splitted.length ? makeList(sSplitted, s1Splitted) : makeList(s1Splitted, sSplitted);
+            String[] json1Splitted = splitJson(json1);
+            String[] json2Splitted = splitJson(json2);
+            //String[] sSplitted = json1.split("\\r?\\n");
+            //String[] s1Splitted = json2.split("\\r?\\n");
+            return json1Splitted.length > json2Splitted.length ? makeList(json1Splitted, json2Splitted) : makeList(json2Splitted, json1Splitted);
         }
     }
 
-    private List<Integer> makeList(String[] sSplitted, String[] s1Splitted) {
+    private List<Integer> makeList(String[] json1Splitted, String[] json2Splitted) {
         ArrayList<Integer> differences = new ArrayList<Integer>();
-        for (int i = 0; i < sSplitted.length; i++) {
-            if (i < s1Splitted.length) {
-                if (!sSplitted[i].equals(s1Splitted[i]))
+        for (int i = 0; i < json1Splitted.length; i++) {
+            if (i < json2Splitted.length) {
+                if (!json1Splitted[i].equals(json2Splitted[i]))
                     differences.add(i + 1);
             } else
                 differences.add(i + 1);
