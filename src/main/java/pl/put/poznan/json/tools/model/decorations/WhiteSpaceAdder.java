3f26ff68550f201302d0d5e3d98ea8cd15f0e634
Iwo Naglik <iwonaglik@o2.pl>
2019-11-25 23:53:44 +0100

    Add javaDocs comment

--- src/main/java/pl/put/poznan/json/tools/model/decorations/WhiteSpaceAdder.java
+++ src/main/java/pl/put/poznan/json/tools/model/decorations/WhiteSpaceAdder.java
@@ -8,21 +8,49 @@ import pl.put.poznan.json.tools.service.StringToJsonParser;
 import pl.put.poznan.json.tools.service.WrongInputException;
 
 
+/**
+ * This is one of the decoration class, which add white spaces from json and returns file as string
+ */
 public class WhiteSpaceAdder extends JsonDecorator {
 
+    /**
+     * Constructor method
+     *
+     * @param jsonObject object of interface JsonObject
+     */
     public WhiteSpaceAdder(JsonObject jsonObject) {
         super(jsonObject);
     }
 
+    /**
+     * This method firstly remove unnecessary white spaces from JSONObject by execute removeWhiteSpaces method
+     *  and then add white spaces to JSONObject by executing addWhiteSpaces method
+     *
+     * @return json file as string without given attributes
+     * @throws WrongInputException
+     */
     public String getJson() throws WrongInputException {
         String minificatedJson = removeWhiteSpaces(jsonObject.getJson());
         return addWhiteSpaces(minificatedJson);
     }
 
+    /**
+     * * This method remove all white spaces from JSONObject by external Validator
+     *
+     * @param json string from which removed white spaces
+     * @return string without white spaces
+     * @throws WrongInputException
+     */
     private String removeWhiteSpaces(String json) throws WrongInputException {
         return this.stringToJsonParser.parse(json).toString();
     }
 
+    /**
+     * This method add white spaces to JSONObject
+     *
+     * @param json string which is modified by add white spaces
+     * @return JSONObject as string with white spaces
+     */
     private String addWhiteSpaces(String json) {
         return json
                 .replaceAll("\":", "\": ")
--- src/main/java/pl/put/poznan/json/tools/model/decorations/WhiteSpaceRemover.java
+++ src/main/java/pl/put/poznan/json/tools/model/decorations/WhiteSpaceRemover.java
@@ -8,16 +8,38 @@ import pl.put.poznan.json.tools.service.StringToJsonParser;
 import pl.put.poznan.json.tools.service.WrongInputException;
 
 
+/**
+ * This is one of the decoration class, which remove white spaces from json and returns file as string
+ */
 public class WhiteSpaceRemover extends JsonDecorator {
 
+    /**
+     * Constructor method
+     *
+     * @param jsonObject object of interface JsonObject
+     */
     public WhiteSpaceRemover(JsonObject jsonObject) {
         super(jsonObject);
     }
 
+    /**
+     * This method collect JSONObject as string and remove white spaces
+     * by executing removeWhiteSpaces method
+     *
+     * @return json file as string without white spaces
+     * @throws WrongInputException
+     */
     public String getJson() throws WrongInputException {
         return removeWhiteSpaces(jsonObject.getJson());
     }
 
+    /**
+     * * This method remove all white spaces from JSONObject by external Validator
+     *
+     * @param json string from which removed white spaces
+     * @return string without white spaces
+     * @throws WrongInputException
+     */
     private String removeWhiteSpaces(String json) throws WrongInputException {
         return this.stringToJsonParser.parse(json).toString();
     }
