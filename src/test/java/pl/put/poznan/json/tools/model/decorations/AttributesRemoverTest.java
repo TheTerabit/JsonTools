484c272338e3309a152d32e9f8b660c2758ed87b
Bartosz Przybyl <bartolomeo369@gmail.com>
2019-12-27 14:12:21 +0100

    Add some unit tests and edit gitignore

--- src/test/java/pl/put/poznan/json/tools/model/decorations/AttributesRemoverTest.java
+++ src/test/java/pl/put/poznan/json/tools/model/decorations/AttributesRemoverTest.java
@@ -1,12 +1,43 @@
 package pl.put.poznan.json.tools.model.decorations;
 
 import org.junit.jupiter.api.Test;
+import pl.put.poznan.json.tools.model.RawJson;
+import pl.put.poznan.json.tools.service.WrongInputException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AttributesRemoverTest {
 
     @Test
-    void getJson() {
+    void removeZeroAttributesFromJson_ReturnedOriginalJson() throws WrongInputException {
+        String json = "{\"name\": \"bartek\",\t\"surname\": \"kowalski\"}";
+        String[] attributes = {};
+        String expectedJson = "{\"surname\":\"kowalski\",\"name\":\"bartek\"}";
+
+        AttributesRemover attributesRemover = new AttributesRemover(new RawJson(json), attributes);
+
+        assertEquals(attributesRemover.getJson(), expectedJson);
+    }
+
+    @Test
+    void removeAllAttributesFromJson_ReturnedEmptyJson() throws WrongInputException {
+        String json = "{\"name\": \"bartek\",\t\"surname\": \"kowalski\"}";
+        String[] attributes = {"name", "surname"};
+        String expectedJson = "{}";
+
+        AttributesRemover attributesRemover = new AttributesRemover(new RawJson(json), attributes);
+
+        assertEquals(attributesRemover.getJson(), expectedJson);
+    }
+
+    @Test
+    void removeOneAttributeFromJson_ReturnedJsonWithoutOneAttribute() throws WrongInputException {
+        String json = "{\"name\": \"bartek\",\t\"surname\": \"kowalski\"}";
+        String[] attributes = {"name"};
+        String expectedJson = "{\"surname\":\"kowalski\"}";
+
+        AttributesRemover attributesRemover = new AttributesRemover(new RawJson(json), attributes);
+
+        assertEquals(attributesRemover.getJson(), expectedJson);
     }
 }
