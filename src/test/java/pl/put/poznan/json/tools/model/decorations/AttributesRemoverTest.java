9fe80224506479d3a951a515adc20e25a9466e66
BartekPrz <38264818+BartekPrz@users.noreply.github.com>
2020-01-13 13:26:46 +0100

    Bartekprz (#31)
    * add version tag in pom.xml
    
    * ComparisonServiceTest + WhiteSpaceAdderTest
    
    * Add some unit tests and edit gitignore
    
    * add some tests
    
    * add nullRemoverTest
    
    * add 4 tests for nullremover class
    
    * add 7 tests with mocks for JsonToolsService
    
    * Dev (#30)
    
    * add version tag in pom.xml
    
    * add some tests
    
    * add nullRemoverTest
    
    * add 4 tests for nullremover class
    
    * add 7 tests with mocks for JsonToolsService
    
    Co-authored-by: Pefes <56848101+Pefes@users.noreply.github.com>
    Co-authored-by: NaIwo <38052250+NaIwo@users.noreply.github.com>
    
    * Delete io-project-architecture-0.1.jar
    
    * Delete io-project-architecture-0.1.jar.original
    
    * Delete io-project-architecture-1.0.0.jar
    
    * Delete io-project-architecture-1.0.0.jar.original
    
    * Delete JsonToolsControllerTest.class
    
    * Delete AttributesPickerTest.class
    
    * Delete AttributesRemoverTest.class
    
    * Delete NullRemoverTest.class
    
    * Delete WhiteSpaceAdderTest.class
    
    * Delete WhiteSpaceRemoverTest.class
    
    * Delete ComparisonServiceTest.class
    
    * Delete JsonToolsServiceTest.class
    
    * Delete ParametersValidatorTest.class
    
    * Delete StringToJsonParserTest.class
    
    * Add 4 additinal tests with mocks
    
    * Remove .class files
    
    * Update javadoc and jar
    
    Co-authored-by: Pefes <56848101+Pefes@users.noreply.github.com>
    Co-authored-by: Bartosz Świtaj <36801835+TheTerabit@users.noreply.github.com>
    Co-authored-by: NaIwo <38052250+NaIwo@users.noreply.github.com>

--- src/test/java/pl/put/poznan/json/tools/model/decorations/AttributesRemoverTest.java
+++ src/test/java/pl/put/poznan/json/tools/model/decorations/AttributesRemoverTest.java
@@ -1,12 +1,74 @@
 package pl.put.poznan.json.tools.model.decorations;
 
 import org.junit.jupiter.api.Test;
+import pl.put.poznan.json.tools.model.RawJson;
+import pl.put.poznan.json.tools.service.StringToJsonParser;
+import pl.put.poznan.json.tools.service.WrongInputException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
 
 class AttributesRemoverTest {
 
     @Test
-    void getJson() {
+    void removeZeroAttributesFromJson_ReturnedOriginalJson() throws WrongInputException {
+        String json = "{\"name\": \"bartek\",\t\"surname\": \"kowalski\"}";
+        String[] attributes = {};
+        String expectedJson = "{\"surname\":\"kowalski\",\"name\":\"bartek\"}";
+
+        AttributesRemover attributesRemover = new AttributesRemover(new RawJson(json), attributes);
+
+        assertEquals(attributesRemover.getJson(), expectedJson);
+    }
+
+    @Test
+    void removeAllAttributesFromJson_ReturnedEmptyJson() throws WrongInputException {
+        String json = "{\"name\": \"bartek\",\t\"surname\": \"kowalski\"}";
+        String[] attributes = {"name", "surname"};
+        String expectedJson = "{}";
+
+        AttributesRemover attributesRemover = new AttributesRemover(new RawJson(json), attributes);
+
+        assertEquals(attributesRemover.getJson(), expectedJson);
+    }
+
+    @Test
+    void removeOneAttributeFromJson_ReturnedJsonWithoutOneAttribute() throws WrongInputException {
+        String json = "{\"name\": \"bartek\",\t\"surname\": \"kowalski\"}";
+        String[] attributes = {"name"};
+        String expectedJson = "{\"surname\":\"kowalski\"}";
+
+        AttributesRemover attributesRemover = new AttributesRemover(new RawJson(json), attributes);
+
+        assertEquals(attributesRemover.getJson(), expectedJson);
+    }
+
+    @Test
+    void removeTwoAttributesFromJson_ReturnedJsonWithoutTwoAttributes() throws WrongInputException {
+        StringToJsonParser stringToJsonParser = mock(StringToJsonParser.class, RETURNS_DEEP_STUBS);
+
+        String json = "{\"name\": \"bartek\",\t\"surname\": \"kowalski\",\t\"age\": \"21\"}";
+        String minificatedJson = "{\"name\":\"bartek\",\"surname\":\"kowalski\",\"age\":\"21\"}";
+        String[] attributes = {"surname", "age"};
+        String expectedJson = "{\"name\":\"bartek\"}";
+
+        AttributesRemover attributesRemover = new AttributesRemover(new RawJson(json), attributes);
+        when(stringToJsonParser.parse(any(String.class)).toString()).thenReturn(minificatedJson);
+        assertEquals(attributesRemover.getJson(), expectedJson);
+    }
+
+    @Test
+    void removeThreeAttributesFromJson_ReturnedJsonWithoutThreeAttributes() throws WrongInputException {
+        StringToJsonParser stringToJsonParser = mock(StringToJsonParser.class, RETURNS_DEEP_STUBS);
+
+        String json = "{\"name\": \"bartek\",\t\"surname\": \"kowalski\",\t\"age\": \"21\",\t\"type\": \"int\"}";
+        String minificatedJson = "{\"name\":\"bartek\",\"surname\":\"kowalski\",\"age\":\"21\",\"type\":\"int\"}";
+        String[] attributes = {"surname", "age", "name"};
+        String expectedJson = "{\"type\":\"int\"}";
+
+        AttributesRemover attributesRemover = new AttributesRemover(new RawJson(json), attributes);
+        when(stringToJsonParser.parse(any(String.class)).toString()).thenReturn(minificatedJson);
+        assertEquals(attributesRemover.getJson(), expectedJson);
     }
 }
--- src/test/java/pl/put/poznan/json/tools/model/decorations/WhiteSpaceAdderTest.java
+++ src/test/java/pl/put/poznan/json/tools/model/decorations/WhiteSpaceAdderTest.java
@@ -1,12 +1,102 @@
 package pl.put.poznan.json.tools.model.decorations;
 
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.Test;
+import pl.put.poznan.json.tools.model.RawJson;
+import pl.put.poznan.json.tools.service.StringToJsonParser;
+import pl.put.poznan.json.tools.service.WrongInputException;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class WhiteSpaceAdderTest {
 
     @Test
-    void getJson() {
+    void addSpacesToCasualJson() throws WrongInputException {
+
+        StringToJsonParser stringToJsonParser = mock(StringToJsonParser.class, RETURNS_DEEP_STUBS);
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("surname","kowalski");
+        jsonObject.put("name","bartek");
+
+        String json = "{\"name\": \"bartek\",\t\"surname\": \"kowalski\"}";
+        String minificatedJson = "{\"name\":\"bartek\",\"surname\":\"kowalski\"}";
+        String expectedJson = "{\n\t\"name\": \"bartek\",\n\t\"surname\": \"kowalski\"\n}";
+
+        WhiteSpaceAdder whiteSpaceAdder = new WhiteSpaceAdder(new RawJson(json));
+        whiteSpaceAdder.setStringToJsonParser(stringToJsonParser);
+
+        when(stringToJsonParser.parse(any(String.class)).toString()).thenReturn(minificatedJson);
+
+        assertEquals(whiteSpaceAdder.getJson(),expectedJson);
+        verify(stringToJsonParser.parse(json), times(1));
+    }
+
+
+    @Test
+    void addSpacesToEmptyJson() throws WrongInputException {
+
+        StringToJsonParser stringToJsonParser = mock(StringToJsonParser.class, RETURNS_DEEP_STUBS);
+
+        JSONObject jsonObject = new JSONObject();
+
+        String json = "{\n}";
+        String minificatedJson = "{}";
+        String expectedJson = "{\n" +
+                "\t\n" +
+                "}";
+
+        WhiteSpaceAdder whiteSpaceAdder = new WhiteSpaceAdder(new RawJson(json));
+        whiteSpaceAdder.setStringToJsonParser(stringToJsonParser);
+
+        when(stringToJsonParser.parse(any(String.class)).toString()).thenReturn(minificatedJson);
+
+        assertEquals(whiteSpaceAdder.getJson(),expectedJson);
+        verify(stringToJsonParser.parse(json), times(1));
+    }
+
+    @Test
+    void addSpacesToJsonInJson() throws WrongInputException {
+
+        StringToJsonParser stringToJsonParser = mock(StringToJsonParser.class, RETURNS_DEEP_STUBS);
+
+        JSONObject jsonObject = new JSONObject();
+
+        String json = "{\"firstName\":\"John\",\"lastName\":\"Smith\",\"isAlive\":true,\"address\":{\"streetAddress\":\"21 2nd Street\",\"city\":\"New York\",\"postalCode\":\"10021-3100\",\"state\":\"NY\"},\"children\":[],\"age\":27,\"phoneNumbers\":[{\"number\":\"212 555-1234\",\"type\":\"home\"},{\"number\":\"646 555-4567\",\"type\":\"office\"},{\"number\":\"123 456-7890\",\"type\":\"mobile\"}],\"spouse\":null}";
+        String minificatedJson = "{\"firstName\":\"John\",\"lastName\":\"Smith\",\"isAlive\":true,\"address\":{\"streetAddress\":\"21 2nd Street\",\"city\":\"New York\",\"postalCode\":\"10021-3100\",\"state\":\"NY\"},\"children\":[],\"age\":27,\"phoneNumbers\":[{\"number\":\"212 555-1234\",\"type\":\"home\"},{\"number\":\"646 555-4567\",\"type\":\"office\"},{\"number\":\"123 456-7890\",\"type\":\"mobile\"}],\"spouse\":null}";
+        String expectedJson = "{\n" +
+                "\t\"firstName\": \"John\",\n" +
+                "\t\"lastName\": \"Smith\",\n" +
+                "\t\"isAlive\": true,\"address\": {\n" +
+                "\t\"streetAddress\": \"21 2nd Street\",\n" +
+                "\t\"city\": \"New York\",\n" +
+                "\t\"postalCode\": \"10021-3100\",\n" +
+                "\t\"state\": \"NY\"\n" +
+                "\t},\n" +
+                "\"children\": [],\"age\": 27,\"phoneNumbers\": [{\n" +
+                "\t\"number\": \"212 555-1234\",\n" +
+                "\t\"type\": \"home\"\n" +
+                "\t},\n" +
+                "\n" +
+                "\t{\n" +
+                "\t\"number\": \"646 555-4567\",\n" +
+                "\t\"type\": \"office\"\n" +
+                "\t},\n" +
+                "\n" +
+                "\t{\n" +
+                "\t\"number\": \"123 456-7890\",\n" +
+                "\t\"type\": \"mobile\"\n" +
+                "\t}],\n" +
+                "\t\"spouse\": null\n" +
+                "}";
+
+        WhiteSpaceAdder whiteSpaceAdder = new WhiteSpaceAdder(new RawJson(json));
+        whiteSpaceAdder.setStringToJsonParser(stringToJsonParser);
+
+        when(stringToJsonParser.parse(any(String.class)).toString()).thenReturn(minificatedJson);
+
+        assertEquals(whiteSpaceAdder.getJson(),expectedJson);
+        verify(stringToJsonParser.parse(json), times(1));
     }
 }
