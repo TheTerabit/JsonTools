dfa3467a659704380915d91afe728f8df4f3f701
Bartosz Przybyl <bartolomeo369@gmail.com>
2020-01-13 13:14:29 +0100

    Add 4 additinal tests with mocks

--- src/test/java/pl/put/poznan/json/tools/model/decorations/AttributesPickerTest.java
+++ src/test/java/pl/put/poznan/json/tools/model/decorations/AttributesPickerTest.java
@@ -2,9 +2,12 @@ package pl.put.poznan.json.tools.model.decorations;
 
 import org.junit.jupiter.api.Test;
 import pl.put.poznan.json.tools.model.RawJson;
+import pl.put.poznan.json.tools.service.StringToJsonParser;
 import pl.put.poznan.json.tools.service.WrongInputException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
 
 class AttributesPickerTest {
 
@@ -41,4 +44,31 @@ class AttributesPickerTest {
         assertEquals(attributesPicker.getJson(), expectedJson);
     }
 
+    @Test
+    void getTwoAttributesFromJson_ReturnedJsonWithTwoAttributes() throws WrongInputException {
+        StringToJsonParser stringToJsonParser = mock(StringToJsonParser.class, RETURNS_DEEP_STUBS);
+
+        String json = "{\"name\": \"bartek\",\t\"surname\": \"kowalski\",\t\"age\": \"21\"}";
+        String minificatedJson = "{\"name\":\"bartek\",\"surname\":\"kowalski\",\"age\":\"21\"}";
+        String[] attributes = {"surname", "age"};
+        String expectedJson = "{\n\t\"surname\": \"kowalski\"\n\t\"age\": \"21\"\n}";
+
+        AttributesPicker attributesPicker = new AttributesPicker(new RawJson(json), attributes);
+        when(stringToJsonParser.parse(any(String.class)).toString()).thenReturn(minificatedJson);
+        assertEquals(attributesPicker.getJson(), expectedJson);
+    }
+
+    @Test
+    void getThreeAttributesFromJson_ReturnedJsonWithThreeAttributes() throws WrongInputException {
+        StringToJsonParser stringToJsonParser = mock(StringToJsonParser.class, RETURNS_DEEP_STUBS);
+
+        String json = "{\"name\": \"bartek\",\t\"surname\": \"kowalski\",\t\"age\": \"21\",\t\"type\": \"int\"}";
+        String minificatedJson = "{\"name\":\"bartek\",\"surname\":\"kowalski\",\"age\":\"21\",\"type\":\"int\"}";
+        String[] attributes = {"surname", "age", "name"};
+        String expectedJson = "{\n\t\"surname\": \"kowalski\"\n\t\"age\": \"21\"\n\t\"name\": \"bartek\"\n}";
+
+        AttributesPicker attributesPicker = new AttributesPicker(new RawJson(json), attributes);
+        when(stringToJsonParser.parse(any(String.class)).toString()).thenReturn(minificatedJson);
+        assertEquals(attributesPicker.getJson(), expectedJson);
+    }
 }
--- src/test/java/pl/put/poznan/json/tools/model/decorations/AttributesRemoverTest.java
+++ src/test/java/pl/put/poznan/json/tools/model/decorations/AttributesRemoverTest.java
@@ -2,9 +2,12 @@ package pl.put.poznan.json.tools.model.decorations;
 
 import org.junit.jupiter.api.Test;
 import pl.put.poznan.json.tools.model.RawJson;
+import pl.put.poznan.json.tools.service.StringToJsonParser;
 import pl.put.poznan.json.tools.service.WrongInputException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
 
 class AttributesRemoverTest {
 
@@ -38,6 +41,34 @@ class AttributesRemoverTest {
 
         AttributesRemover attributesRemover = new AttributesRemover(new RawJson(json), attributes);
 
+        assertEquals(attributesRemover.getJson(), expectedJson);
+    }
+
+    @Test
+    void removeTwoAttributesFromJson_ReturnedJsonWithoutTwoAttributes() throws WrongInputException {
+        StringToJsonParser stringToJsonParser = mock(StringToJsonParser.class, RETURNS_DEEP_STUBS);
+
+        String json = "{\"name\": \"bartek\",\t\"surname\": \"kowalski\",\t\"age\": \"21\"}";
+        String minificatedJson = "{\"name\":\"bartek\",\"surname\":\"kowalski\",\"age\":\"21\"}";
+        String[] attributes = {"surname", "age"};
+        String expectedJson = "{\"name\":\"bartek\"}";
+
+        AttributesRemover attributesRemover = new AttributesRemover(new RawJson(json), attributes);
+        when(stringToJsonParser.parse(any(String.class)).toString()).thenReturn(minificatedJson);
+        assertEquals(attributesRemover.getJson(), expectedJson);
+    }
+
+    @Test
+    void removeThreeAttributesFromJson_ReturnedJsonWithoutThreeAttributes() throws WrongInputException {
+        StringToJsonParser stringToJsonParser = mock(StringToJsonParser.class, RETURNS_DEEP_STUBS);
+
+        String json = "{\"name\": \"bartek\",\t\"surname\": \"kowalski\",\t\"age\": \"21\",\t\"type\": \"int\"}";
+        String minificatedJson = "{\"name\":\"bartek\",\"surname\":\"kowalski\",\"age\":\"21\",\"type\":\"int\"}";
+        String[] attributes = {"surname", "age", "name"};
+        String expectedJson = "{\"type\":\"int\"}";
+
+        AttributesRemover attributesRemover = new AttributesRemover(new RawJson(json), attributes);
+        when(stringToJsonParser.parse(any(String.class)).toString()).thenReturn(minificatedJson);
         assertEquals(attributesRemover.getJson(), expectedJson);
     }
 }
